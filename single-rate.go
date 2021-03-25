604f0626a131764d0af7a62e295b824a3385bb7a
Morrocker <i.romero.hinrich@gmail.com>
2021-03-25 11:17:26 -0300

    debug functions improved

--- single-rate.go
+++ single-rate.go
@@ -4,6 +4,8 @@ import (
 	"container/list"
 	"sync"
 	"time"
+
+	"github.com/morrocker/log"
 )
 
 type SRate struct {
@@ -12,6 +14,13 @@ type SRate struct {
 	list       *list.List
 
 	lock sync.Mutex
+}
+
+var Logger *log.Logger = log.New()
+
+func init() {
+	Logger.OutputFile("tracker.debug.log")
+	Logger.StartWriter()
 }
 
 func NewSRate(n int) *SRate {
@@ -29,6 +38,7 @@ func (s *SRate) MeasureStart(x int64) func(int64) {
 		diff2 := m - x
 		rate := diff2 * 1000000000 / diff
 		s.add(rate)
+		Logger.Bench("Start: %d | End: %d | Rate: %d | Avg: %d | RateTot: %d / SampleSize: %d", x, m, rate, s.AvgRate(), s.list.Len())
 	}
 }
 
